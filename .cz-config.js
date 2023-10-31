7c5112906327a81831049f820fa5c07674d0d5af
Douglas Espindola <douglas.spindola15@gmail.com>
2023-10-30 23:45:30 -0300

    chore: adicionado cz para exibir icones nos commits semanticos

---
+++ .cz-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  types: [
+    { value: "feat", name: "feat:     🎸  Nova funcionalidade" },
+    { value: "fix", name: "fix:      🐛  Correção de bug" },
+    { value: "docs", name: "docs:     📚  Documentação" },
+    { value: "style", name: "style:    💄  Mudanças de estilo" },
+    { value: "refactor", name: "refactor: 🔨  Refatoração de código" },
+    { value: "test", name: "test:     🚨  Adição de testes" },
+    { value: "chore", name: "chore:    🔧  Tarefas diversas" },
+    { value: "build", name: "build:    🚀  Mudanças no sistema de build" },
+    { value: "ci", name: "ci:       🔬  Mudanças em configurações de CI/CD" },
+  ],
+};
