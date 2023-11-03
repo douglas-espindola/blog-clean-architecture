f61ca8c57ae50df815eb76e0eba78bf4d3dc7a5b
Douglas Espindola <douglas.spindola15@gmail.com>
2023-11-02 23:45:30 -0300

    feat: criado conteudo da model post e criado relacionamento na model user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,50 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\HasSlug;
+use App\Shared\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use UuidTrait;
+    use HasSlug;
+    use SoftDeletes;
     use HasFactory;
+
+    protected $table = 'posts';
+
+    /**
+     * @var string
+     */
+    protected string $model = Post::class;
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'description',
+        'published',
+        'user_id',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'published' => 'boolean'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Shared\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -70,4 +71,12 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
