aab0c642ac5c23da09040a3807d6773a51df79d4
Douglas Espindola <douglas.spindola15@gmail.com>
2023-11-22 23:45:30 -0300

    feat: adicionado route grupo para post

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('posts')->as('posts:')->group(function (){
+    Route::get('/', [\App\Http\Controllers\Api\V1\Posts\IndexController::class, 'index'])->name('index'); //route('api:posts:index')
+});
