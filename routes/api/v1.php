6cef209f05df733300b39cebf304371d15467510
Douglas Espindola <douglas.spindola15@gmail.com>
2023-11-30 01:04:57 -0300

    refactor: criado endpoint de posts

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+    use App\Http\Controllers\Api\V1\Posts\DeleteController;
+    use App\Http\Controllers\Api\V1\Posts\IndexController;
+    use App\Http\Controllers\Api\V1\Posts\ShowController;
+    use App\Http\Controllers\Api\V1\Posts\StoreController;
+    use App\Http\Controllers\Api\V1\Posts\UpdateController;
+    use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+/**
+ * Post Endpoints
+*/
 Route::prefix('posts')->as('posts:')->group(function (){
-    Route::get('/', [\App\Http\Controllers\Api\V1\Posts\IndexController::class, 'index'])->name('index'); //route('api:posts:index')
+    Route::get('/', IndexController::class)->name('index');
+    Route::post('/', StoreController::class)->name('store');
+    Route::get('{post:id}', ShowController::class)->name('show');
+    Route::patch('{post:id}', UpdateController::class)->name('update');
+    Route::delete('{post:id}', DeleteController::class)->name('delete');
 });
